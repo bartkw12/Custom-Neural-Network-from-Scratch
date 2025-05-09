ea001eaf0f24428139caec8086afe516db22f2f1
Bart Kowal <bartkw12@gmail.com>
2025-05-09 14:35:17 -0400

    Add config.py to CustomNN directory

---
+++ CustomNN/config.py
@@ -0,0 +1,34 @@
+# Training Parameters
+BATCH_SIZE = 256
+LEARNING_RATE = 0.002
+EPOCHS = 50
+LAMBDA = 0.001
+
+# Model Parameters
+INPUT_SIZE = 784
+OUTPUT_SIZE = 10
+HIDDEN_UNITS = 80
+HIDDEN_LAYERS = 4
+
+# Technique Parameters
+
+# For Early Stopping
+PATIENCE = 5
+MIN_DELTA = 1e-5
+
+# For Dropout
+DROPOUT_RATE_INPUT = 0.10
+DROPOUT_RATE_HIDDEN = 0.3
+
+# For Batch Normalization
+MOMENTUM = 0.185
+EPSILON = 1e-5
+
+# For ADAM optimizer
+DECAY = 5e-7
+EPSILON_A = 1e-7
+BETA1 = 0.9
+BETA2 = 0.999
+
+# Other Settings
+SEED = 9782
