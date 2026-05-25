57fa8be6c228fe979975912b930dd0a42ed5a7ac
Bartkw12 <bartkw12@gmail.com>
2026-05-24 19:19:39 -0500

    adding init file for custom NN.

---
+++ src/CustomNN/__init__.py
@@ -0,0 +1,4 @@
+from .model import Layer_Dense, Activation_ReLU, Activation_Softmax, Categorical_Cross_entropy_loss
+from .techniques import ADAM_Optimizer, Early_Stopping, Dropout, Batch_Normalization
+from .data_preprocessing import load_fashion_MNIST, preprocess_data
+from . import config
