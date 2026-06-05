2cffa576b757f8a7755d22164f9088d33fe7f431
Bartkw12 <bartkw12@gmail.com>
2026-06-05 15:31:41 -0500

    wired up src/pytorch_nn/__main__.py so the command python -m pytorch_nn works.

--- src/pytorch_nn/__init__.py
+++ src/pytorch_nn/__init__.py
@@ -1,1 +1,28 @@
 """PyTorch comparison package."""
+
+from .compare import load_comparison_histories, load_pytorch_history
+from .model import FashionMNISTNet
+from .train import (
+	build_lr_scheduler,
+	build_optimizer,
+	evaluate_test_set,
+	get_device,
+	prepare_dataloaders,
+	save_history,
+	seed_everything,
+	train_model,
+)
+
+__all__ = [
+	"FashionMNISTNet",
+	"get_device",
+	"seed_everything",
+	"build_lr_scheduler",
+	"build_optimizer",
+	"prepare_dataloaders",
+	"train_model",
+	"evaluate_test_set",
+	"save_history",
+	"load_pytorch_history",
+	"load_comparison_histories",
+]
